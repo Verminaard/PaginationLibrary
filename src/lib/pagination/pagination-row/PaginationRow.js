0662620490067b6b7c9d63067754cdd211e6cbf4
Baryshnikov Oleg <ob@digipeople.ru>
2020-06-11 11:46:38 +0300

    Init commit

---
+++ src/lib/pagination/pagination-row/PaginationRow.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import TableInfo from '../table-info/TableInfo';
+import SizePerPage from '../size-per-page/SizePerPage';
+import PaginationList from '../pagination-list/PaginationList';
+import Const from "../../../index";
+
+const PaginationRow = (props) => (
+   <div className="row">
+      <div className="col-xs-7 pull-left">
+         <TableInfo page={props.page} sizePerPage={props.sizePerPage} itemCount={props.itemCount} />
+         {props.itemCount > props.sizePerPage && (
+            <PaginationList
+               currPage={props.page}
+               sizePerPage={props.sizePerPage}
+               changePage={props.onChangePage}
+               pageStartIndex={props.pageStartIndex}
+               paginationSize={props.paginationSize || Const.PAGINATION_SIZE}
+               dataSize={props.itemCount}
+               prePage={props.prePage || Const.PRE_PAGE}
+               nextPage={props.nextPage || Const.NEXT_PAGE}
+               firstPage={props.firstPage || Const.FIRST_PAGE}
+               lastPage={props.lastPage || Const.LAST_PAGE}
+            />
+         )}
+      </div>
+      <div className="col-xs-5">
+         {props.itemCount > Const.SIZE_PER_PAGE && (
+            <div className="col-xs-12">
+               <div className="pull-right">
+                  <div>
+                     <SizePerPage
+                        onChange={props.onChangeSizePerPage}
+                        sizePerPage={props.sizePerPage}
+                     />
+                     <span>&nbsp;записей на странице</span>
+                  </div>
+               </div>
+            </div>
+         )}
+      </div>
+   </div>
+);
+
+export default PaginationRow;
---
+++ src/lib/pagination/size-per-page/SimpleSelect.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import uniqueId from 'lodash/uniqueId';
+/**
+ * Компонент-обертка для простого select, не требует внешних зависимостей
+ * */
+class SimpleSelect extends Component {
+   constructor(props, context) {
+      super(props, context);
+      this.state = { value: this.props.value };
+      this.handleChange = this.handleChange.bind(this);
+   }
+
+   handleChange(event) {
+      this.setState({ value: event.target.value });
+      this.props.onChange && this.props.onChange(event.target.value);
+   }
+
+   render() {
+      const {
+         className,
+         id,
+         placeholder,
+         notClearableOptions,
+         options,
+         idType,
+         valueType
+      } = this.props;
+
+      return (
+         <select
+            className={className}
+            id={id}
+            onChange={this.handleChange}
+            value={this.state.value}
+            placeholder={placeholder || 'Выберите из списка...'}>
+            {!notClearableOptions && <option />}
+            {options &&
+               options.map((option, index) => (
+                  <option key={uniqueId()} value={option[idType || 'id']}>
+                     {option[valueType || 'name']}
+                  </option>
+               ))}
+         </select>
+      );
+   }
+}
+
+export default SimpleSelect;
